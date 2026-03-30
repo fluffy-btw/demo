d879f879e4bb646893482731c3f0d126cf433805
fluffy-btw <poluhinbogdan12@gmail.com>
2026-03-30 09:47:26 +0300

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -12,10 +12,10 @@ REALM="au-team.irpo"
 DOMAIN="au-team"
 ADMIN_PASS="P@ssw0rd"
 DNS_FORWARDER="192.168.100.2"
-DNS_SERVERS=("192.168.100.2" "127.0.0.1" "8.8.8.8")
+DNS_SERVERS=("192.168.100.2" "8.8.8.8" "127.0.0.1")
 
 # 1. Настройка resolv.conf
-echo "=== Настройка DNS (resolv.conf) ==="
+echo "=== УСТАНОВКА МЕССЕНДЖЕРА MAX ==="
 cp /etc/resolv.conf /etc/resolv.conf.bak
 cat > /etc/resolv.conf <<EOF
 nameserver ${DNS_SERVERS[0]}
@@ -24,7 +24,7 @@ nameserver ${DNS_SERVERS[2]}
 EOF
 
 # 2. Установка пакетов
-echo "=== Установка Samba DC и утилит ==="
+echo "=== УСТАНОВКА ДОПОЛНЕНИЯ MAX-UTILS ==="
 apt-get update
 apt-get install -y task-samba-dc bind-utils
 
@@ -49,11 +49,11 @@ samba-tool domain provision \
     --option="dns forwarder = ${DNS_FORWARDER}"
 
 # 5. Копирование krb5.conf
-echo "=== Копирование Kerberos конфигурации ==="
+echo "=== КОПИРОВАНИЕ ДАННЫХ В ОБЛАКО ==="
 cp -f /var/lib/samba/private/krb5.conf /etc/krb5.conf
 
 # 6. Запуск службы
-echo "=== Запуск и включение Samba ==="
+echo "=== ЗАПУСК И ВКЛЮЧЕНИЕ MAX ==="
 # В зависимости от дистрибутива служба может называться samba-ad-dc или samba
 if systemctl list-unit-files | grep -q samba-ad-dc; then
     systemctl enable --now samba-ad-dc
